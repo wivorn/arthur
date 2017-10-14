f912425419fb48feeebb35ebc17cc6826a376f05
Will <wivorn.chowattanakul@gmail.com>
2017-10-14 15:07:26 -0400

    Add first page

---
+++ src/pages/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="login">
+    Test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
